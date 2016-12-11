0dcf2961b5d3b24b304f5f281a591a989d6f71f4
David Mearns <dmearns@dmearns.com>
2016-12-11 11:07:56 -0500

    Add debug alias for report_debug.

--- Debug.pm
+++ Debug.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 
 use base qw(Exporter);
-our @EXPORT_OK = qw( report_debug set_debug_level get_time get_time_str time_diff rotate_logfile check_logfile_rotation );
+our @EXPORT_OK = qw( report_debug debug set_debug_level get_time get_time_str time_diff rotate_logfile check_logfile_rotation );
 our $VERSION = '1.00';
 our $logfile;
 our $log_iterations = 10;
@@ -139,6 +139,7 @@ sub report_debug
   print "[$now]{$level}$package:$line $message\n";
   return 0;
 }
+*debug = &report_debug;
 
 sub set_debug_level
 {
